e5b56431e2a3e4e004b9e6ec3709c8640f4ac468
Cawan Carvalho <cawancarvalho3@gmail.com>
2025-06-29 18:00:31 -0300

    feat: add Select component

---
+++ src/components/Form/Select.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useId } from "react";
+import { useFormContext } from "react-hook-form";
+
+export type SelectProps = {
+  name: string;
+  id: string;
+  label: string;
+  options: Array<string>;
+  rules?: Parameters<ReturnType<typeof useFormContext>["register"]>[1];
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
+
+export const Select = ({
+  name,
+  id,
+  label,
+  options,
+  rules,
+  ...props
+}: SelectProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = errors[name]?.message?.toString();
+  const errorId = useId();
+
+  const { ...restRegister } = register(name, rules);
+
+  return (
+    <div className="flex flex-col gap-0.5">
+      <label className="text-sm text-primary-700 font-semibold" htmlFor={id}>
+        {label}
+      </label>
+      <select
+        className="px-2 py-1 border border-primary-800 rounded-sm text-2xl bg-primary-100 placeholder:text-primary-600 text-primary-800 focus:ring focus ring-primary-800 outline-0"
+        aria-invalid={!!error}
+        aria-describedby={error ? errorId : undefined}
+        id={id}
+        {...restRegister}
+        {...props}
+      >
+        {options.map((option, index) => {
+          if (index == 0) {
+            return <option key={index}>{option}</option>;
+          }
+          return (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+      <span id={errorId} className="text-sm text-red-500 font-semibold">
+        {error}
+      </span>
+    </div>
+  );
+};
